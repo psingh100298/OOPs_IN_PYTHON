f7776aca540d3d1d5dd2cfb9543e1ca2123b2192
Pradeep Singh <psingh10021998@gmail.com>
2023-04-29 18:21:30 +0530

    commit changes

---
+++ student.py
@@ -0,0 +1,17 @@
+my_student = {
+    'name':'Rolf Smith',
+    'grades': [70,88,90,99]
+}
+
+def average_grade(student):
+    return sum(student['grades'])/ len(student['grades'])
+
+class Student:
+    def __init__(self,new_name,new_grades):
+        self.name = new_name
+        self.grades = new_grades
+
+    def average(self):
+        return sum(self.grades)/len(self.grades)
+    
+student_one = Student()
